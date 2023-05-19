fef234c7ae77a0f7acc68d585f438a0d4d4814a1
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-05-19 23:13:51 +0600

    Practice

---
+++ Fundamentals.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main(){
+//    char a;
+//    short b;
+//    printf("Enter a character:\n");
+//    scanf("%c",&a);
+//    printf("Enter a signed short value:\n");
+//    scanf("%hi",&b);
+//    printf("You entered character: %c\n",a);
+//    printf("You entered character: %hi\n",b);
+
+    //// SUM
+//    int a,b;
+//    int sum=0;
+//    printf("Input first number:\n");
+//    scanf("%d",&a);
+//    printf("Input first number:\n");
+//    scanf("%d",&b);
+//    sum=a+b;
+//    printf("Sum:%d",sum);
+
+    ////C program to perform all arithmetic operations
+//    float a,b;
+//    printf("First number: \n");
+//    scanf("%f",&a);
+//    printf("Second number: \n");
+//    scanf("%f",&b);
+//    float sum=a+b;
+//    float difference=a-b;
+//    float product=a*b;
+//    float Quotient=a/b;
+//    int modulus=(int) a % (int) b;
+//
+//    printf("%f\n",sum);
+//    printf("%f\n",difference);
+//    printf("%f\n",product);
+//    printf("%f\n",Quotient);
+//    printf("%f\n",modulus);
+
+        ////C program to find perimeter of a rectangle
+
+
+
+
+
+
+
+
+
+    return 0;
+}
